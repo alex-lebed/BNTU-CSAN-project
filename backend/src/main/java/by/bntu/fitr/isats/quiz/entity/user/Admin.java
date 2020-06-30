b0e471ee3adf10307dc9449c63e05976f7b90f08
Mikhail Loseu <lawseff@gmail.com>
2020-06-30 05:32:43 +0300

    Add JPA annotations to entities

--- backend/src/main/java/by/bntu/fitr/isats/quiz/entity/user/Admin.java
+++ backend/src/main/java/by/bntu/fitr/isats/quiz/entity/user/Admin.java
@@ -6,6 +6,10 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,10 +17,17 @@ import lombok.ToString;
 @EqualsAndHashCode
 @ToString
 @Builder
+@Entity
+@Table(name = "quiz_admin")
 public class Admin {
 
+    @Id
     private String login;
+
+    @Column(name = "admin_password")
     private String password;
+
+    @Column(name = "admin_name")
     private String name;
 
 }
--- backend/src/main/java/by/bntu/fitr/isats/quiz/entity/user/Winner.java
+++ backend/src/main/java/by/bntu/fitr/isats/quiz/entity/user/Winner.java
@@ -5,6 +5,13 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 import java.sql.Timestamp;
 
 @NoArgsConstructor
@@ -12,11 +19,30 @@ import java.sql.Timestamp;
 @Getter
 @EqualsAndHashCode
 @ToString
+@Entity
+@Table(name = "quiz_winner")
 public class Winner {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Transient
+    private Integer localId; // id in the lobby
+
+    @Column(name = "player_name")
     private String playerName;
-    private Timestamp timestamp;
+
+    @Column(name = "game_date")
+    private Timestamp gameDate;
+
     private int score;
+
+    public Winner(Integer localId, String playerName, Timestamp gameDate, int score) {
+        this.localId = localId;
+        this.playerName = playerName;
+        this.gameDate = gameDate;
+        this.score = score;
+    }
 
 }
